7e69848df71873d1041610dd02a2745e89af0ec9
Drew-A <drew.a.anzalone@gmail.com>
2025-04-14 17:42:17 -0700

    card index and home html

--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+class Card:
+    def __init__(self, hint, answer):
+        self.hint = hint
+        self.answer = answer
+
+cards = [
+    Card("What country are you in?", "United States"),
+    Card("How many primary colors are there?", "3"),
+    Card("What century is it?", "21st Century"),
+]
 
 def home(request):
-    return HttpResponse('<h1>Hello Studiers</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
+def card_index(request):
+    return render(request, 'cards/index.html', {"cards": cards})
