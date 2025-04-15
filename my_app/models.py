98d811d0a3ef94af13e1dd2a7d239c17cd0182e6
Drew-A <drew.a.anzalone@gmail.com>
2025-04-14 23:13:15 -0700

    model + table

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    hint = models.TextField(max_length=255)
+    answer = models.CharField(max_length=127)
+    
+    def __str__(self):
+        return f"{self.hint} : {self.answer}"
--- quizzletRemake/settings.py
+++ quizzletRemake/settings.py
@@ -75,8 +75,9 @@ WSGI_APPLICATION = 'quizzletRemake.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quizcards',
+        'PASSWORD': '2123',
     }
 }
 
