861dc4f607bb6c6546c58bf1ee9662d303c64c1e
Drew-A <drew.a.anzalone@gmail.com>
2025-04-15 05:42:30 -0700

    add user foreign key to cards

--- my_app/models.py
+++ my_app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Card(models.Model):
     hint = models.TextField(max_length=255)
     answer = models.CharField(max_length=127)
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"{self.hint} : {self.answer}"
