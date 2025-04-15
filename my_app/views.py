ebc9b6e978f64634123d0a26fef319da8487a55e
Drew-A <drew.a.anzalone@gmail.com>
2025-04-15 08:50:37 -0700

    remove some comments

--- my_app/views.py
+++ my_app/views.py
@@ -58,24 +58,13 @@ def card_detail(request, card_id):
 def signup(request):
     error_message = ''
     if request.method == 'POST':
-        # This is how to create a 'user' form object
-        # that includes the data from the browser
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            # This will add the user to the database
             user = form.save()
-            # This is how we log a user in
             login(request, user)
             return redirect('card-index')
         else:
             error_message = 'Invalid sign up - try again'
-    # A bad POST or a GET request, so render signup.html with an empty form
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'signup.html', context)
-    # Same as: 
-    # return render(
-    #     request, 
-    #     'signup.html',
-    #     {'form': form, 'error_message': error_message}
-    # )
