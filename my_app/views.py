e35a14d9bcb3b2f07b1ac910b37d0a84f650f3a0
Drew-A <drew.a.anzalone@gmail.com>
2025-04-15 04:57:28 -0700

    add view and update to use ORM

--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
+from .models import Card
 
-class Card:
-    def __init__(self, hint, answer):
-        self.hint = hint
-        self.answer = answer
-
-cards = [
-    Card("What country are you in?", "United States"),
-    Card("How many primary colors are there?", "3"),
-    Card("What century is it?", "21st Century"),
-]
+def find_index(arr, call):
+    for idx, obj in enumerate(arr):
+        if call(obj):
+            return idx
+    return -1
 
 def home(request):
     return render(request, 'home.html')
@@ -18,4 +14,20 @@ def about(request):
     return render(request, 'about.html')
 
 def card_index(request):
+    cards = Card.objects.order_by('id')
     return render(request, 'cards/index.html', {"cards": cards})
+
+def card_view(request, card_id):
+    neighbors = [None, None]
+    cards = Card.objects.order_by('id')
+    idx = find_index(cards, lambda x: x.id == card_id)
+
+    # TODO fix assuming valid id
+    if idx == 0 and len(cards) > 1:
+        neighbors[1] = cards[1].id
+    elif idx == len(cards)-1 and len(cards) > 1:
+        neighbors[0] = cards[idx-1].id
+    elif len(cards) > 2:
+        neighbors = [cards[idx-1].id, cards[idx+1].id]
+    
+    return render(request, 'cards/detail.html', {'card': cards[idx], 'prev': neighbors[0], 'next': neighbors[1]})
